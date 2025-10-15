aeef34c21294a1068892c7a8f2a07ac28c912077
devops4sure <jadhavpatil4378@gmail.com>
2025-10-15 15:44:11 +0530

    added subnet,route table,internet gateway

--- main.tf
+++ main.tf
@@ -16,3 +16,32 @@ resource "aws_vpc" "myvpc" {
     cidr_block = "10.10.0.0/16"
     
 }
+
+resource "aws_subnet" "pub_sub" {
+  vpc_id            = aws_vpc.kjmyvpc.id
+  cidr_block        = "10.10.1.0/24"
+  availability_zone = "us-east-1a"
+
+}
+
+resource "aws_internet_gateway" "kj_igw01" {
+  vpc_id = aws_vpc.kjmyvpc.id
+}
+
+
+resource "aws_route_table" "custom_main_rtb" {
+  vpc_id = aws_vpc.kjmyvpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.kj_igw01.id
+
+  }
+
+}
+
+resource "aws_route_table_association" "pub_sub_assoc" {
+  subnet_id = aws_subnet.pub_sub.id
+  route_table_id = aws_route_table.custom_main_rtb.id
+  
+}
